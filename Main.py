4aad4015e4d0b7b80849ad1977f245772c5bc799
AveryGuibord <avery.guibord@ocsbstudent.ca>
2019-02-28 12:55:55 -0500

    did stuff, ye.
    ye.

--- Main.py
+++ Main.py
@@ -11,33 +11,48 @@ change=amount-cost
 print ("Your change is", change)
 hundred=change//100
 change=change%100
-print (hundred, " x $100")
+if hundred>0:
+    print (hundred, " x $100")
 fifty=change//50
 change=change%50
-print (fifty, " x $50")
+if fifty>0:
+    print (fifty, " x $50")
 twenty=change//20
 change=change%20
-print (twenty, " x $20")
+if twenty>0:
+    print (twenty, " x $20")
 ten=change//10
 change=change%10
-print (ten, " x $10")
+if ten>0:
+    print (ten, " x $10")
 five=change//5
 change=change%5
-print (five, " x $5")
+if five>0:
+    print (five, " x $5")
 toonie=change//2
 change=change%2
-print (toonie, " x $2")
+if toonie>0:
+    print (toonie, " x $2")
 one=change//1
 change=change%1
-print (one, " x $1")
+if one>0:
+    print (one, " x $1")
 quarter=change//0.25
 change=change%0.25
-print (quarter, " x $0.25")
+if quarter>0:
+    print (quarter, " x $0.25")
 dime=change//0.10
 change=change%0.10
-print (dime, " x $0.10")
+if dime>0:
+    print (dime, " x $0.10")
 nickle=change//0.05
 change=change%0.05
-print (nickle, " x $0.05")
+if nickle>0:
+    print (nickle, " x $0.05")
+if change<0.03:
+    nickle=nickle + 0.05
+
+
+
 
 
